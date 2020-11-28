6d3965a1e71ed4c19de4e5ac5c5004236b945523
Dmytro Anikin <dimianni@users.noreply.github.com>
2020-11-28 13:29:47 +0100

    Latest Version

--- README.md
+++
@@ -1,2 +0,0 @@
-# gulp_config
-My Gulp Configuration
--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,157 @@
-// Система сборки фронтенда 
-// Качаем Node Js - это программа интерпретатор (переводит JS для машины)
+/*----------------------------------------------------*/
+/*	NOTE:
+/*
+/*  NodeJS has to be installed globally on computer first: https://nodejs.org/en/
+/*  Then, Gulp has to installed globally on computer by running this command in terminal: npm i --global gulp-cli
+/*  For more info, visit Gulp at: https://gulpjs.com/
+------------------------------------------------------*/
+
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 
 
-// Создаем папку и открываем эту папку в терминале
-// Пишем - npm (node packet manager) i gulp-cli -g (global)
-// Потом пишем npm init (открываем новый проект) и прописываем описание (создает package.json)
-// Закидываем папку в редактор, открываем терминал и пишем -> npm i gulp --save-dev
-// Создаем в папке файл gulpfile.js и пишем в ней переменную gulp
-// Создаем папку app, а в ней папки scss js images fonts и index.html
-// Подклчаем в html style.css и main.js 
-// Скачиваем плагин gulp-sass (npm i gulp-sass --save-dev), и в gulpfile.js пишем переменную sass
-// gulp.task (таска) - будет брать файлы scss и компелировать из в css
+/*----------------------------------------------------*/
+/*	Function that:
+/*      - Takes JS files
+/*      - Concatenates them into one file
+/*      - Minimizes the file 
+/*      - Puts the file into 'app/js';
+/*      - Triggers browser update
+/*  NOTE:
+/*      There wil be 2 files in JS folder: 'main.js' & 'main.min.js'
+/*      'main.js' is used when coding and 'main.min.js' is the final file that is being created and linked to HTML.
+------------------------------------------------------*/
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js', // npm i --save-dev jquery
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
 
-// Устанавливаем browser-sync (автоматическое обновление страницы) (npm i browser-sync --save-dev), и пишем для нее переменную в gulpfile.js
-// Скачиваем npm i normalize.css slick-carousel magnific-popup
-// Скачиваем npm i gulp-uglify gulp-concat --save-dev (для минифицирования и конкатинации[соединиения двух файлов в один]), и создаем переменные
-// Подключаем libs.min.js к html
-// Скачиваем npm i gulp-rename --save-dev, и создаем переменную
-
-// Удаляем slick.scss из папки slick в node_modules
-
-// Подключаем к html jquery через cdn
-// В style.scss пишем @import "vars" @import "libs" @import "fonts" @import "media" и в папке scss создаем _vars.scss _libs.scss и тд. _(чтобы не импортировало в css)
-
-// Устанавливаем npm i del --save-dev, создаем переменную; Чтобы папка dist перед тем как билдилась удалялась;
-// Устанавливаем npm i gulp-autoprefixer --save-dev, создаем переменную;
-
-// При новом проекте нам нужно только папка app, gulpfile.js и package.json; В терминале пишем npm i
-
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
-
-
-gulp.task('clean', async function () { //Удаляет папку dist; Async означает что таска будет выполняться первой
-    del.sync('dist')
-});
-
-gulp.task('scss', function () {
-    return gulp.src('app/scss/**/*.scss') //ищем все файлы scss
-        .pipe(sass({ outputStyle: 'compressed' })) //загоняем их в трубу где будет выполняться плагин 
+/*----------------------------------------------------*/
+/*	Function that:
+/*      - Takes SCSS files
+/*      - Compiles them to CSS
+/*      - Minimizes them
+/*      - Concatenates them into one file
+/*      - Adds autoprefixers for better browser compatibility
+/*      - Puts the file into 'app/css';
+/*      - Triggers browser update
+/*    
+/*  NOTE:
+/*      Only style.scss is selected from app folder since all other .scss files are imported there
+------------------------------------------------------*/
+function styles() {
+    return src([
+        'app/scss/style.scss'
+    ])
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
-            overrideBrowserslist: ['last 8 versions']
+            overrideBrowserlist: ['last 10 versions'],
+            grid: true
         }))
-        .pipe(rename({ suffix: '.min' })) //переименовываем
-        .pipe(gulp.dest('app/css')) //выкидываем файлы в папку
-        .pipe(browserSync.reload({ stream: true })) //обновление страницы при изменениях в css
-});
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
 
-gulp.task('css', function () { //Ищем все css плагинов (по аналогии js)
-    return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-    ])
-        .pipe(concat('_libs.scss')) //выполняем конкат
-        .pipe(gulp.dest('app/scss')) //Выкидываем
-        .pipe(browserSync.reload({ stream: true }))
-});
-
-gulp.task('html', function () { //обновление страницы при изменениях в html
-    return gulp.src('app/**/*.html')
-        .pipe(browserSync.reload({ stream: true }))
-});
-
-gulp.task('script', function () { //обновление страницы при изменениях в js
-    return gulp.src('app/js/*.js')
-        .pipe(browserSync.reload({ stream: true }))
-});
-
-gulp.task('js', function () {
-    return gulp.src([ //Ищем все js плагинов
-        'node_modules/slick-carousel/slick/slick.js',
-    ])
-        .pipe(concat('libs.min.js')) //выполняем конкат
-        .pipe(uglify()) //Минифицируем
-        .pipe(gulp.dest('app/js')) //Выкидываем
-        .pipe(browserSync.reload({ stream: true })) //чтобы обновляло js при перезагруке страницы
-});
-
-gulp.task('browser-sync', function () {
+/*----------------------------------------------------*/
+/*	Function that starts a server
+------------------------------------------------------*/
+function browsersync() {
     browserSync.init({
         server: {
-            baseDir: "app/" //директория для сервера
+            baseDir: 'app/'
         }
-    });
-});
+    })
+}
 
-gulp.task('export', function () {
-    let buildHtml = gulp.src('app/**/*.html') //выбираем все файлы для экспорта в папку dist
-        .pipe(gulp.dest('dist'));
+/*----------------------------------------------------*/
+/*	Function that watches for changes in SCSS, JS & HTML
+/*	Every time there is a change in any --> runs 'styles' or 'scripts' function to recompile files
+/*
+/*  NOTE:
+/*      'main.min.js' is excluded. Otherwise it will create an infinite loop, since it is the 'dump' file in 'scripts' function.
+/*      'browserSync.reload' has to be done on HTML changes since there is not function specifically for HTML (unlike 'styles' or 'scripts' functions)
+------------------------------------------------------*/
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+    watch(['app/*.html']).on('change', browserSync.reload)
+}
 
-    let buildCss = gulp.src('app/css/**/*.css')
-        .pipe(gulp.dest('dist/css'));
 
-    let buildJs = gulp.src('app/js/**/*.js')
-        .pipe(gulp.dest('dist/js'));
 
-    let buildFonts = gulp.src('app/fonts/**/*.*')
-        .pipe(gulp.dest('dist/fonts'));
+/*----------------------------------------------------*/
+/*	Function that takes necessary files and puts them into dist folder
+------------------------------------------------------*/
+function build() {
+    return src(
+        [
+            'app/css/style.min.css',
+            'app/fonts/**/*',
+            'app/js/main.min.js',
+            'app/*.html'
+        ],
+        { base: 'app' } // Same file structure as app folder
+    )
+        .pipe(dest('dist'))
+}
 
-    let buildImages = gulp.src('app/images/**/*.*')
-        .pipe(gulp.dest('dist/images'));
-});
+/*----------------------------------------------------*/
+/*	Function that deletes dist folder
+------------------------------------------------------*/
+function cleanDist() {
+    return del('dist')
+}
 
-gulp.task('watch', function () { //встроеная таска которая следит и автоматически выполняет что-то
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss')) //если были внесены изменения запускает scss
-    gulp.watch('app/**/*.html', gulp.parallel('html')) //если были внесены изменения запускает html
-    gulp.watch('app/js/*.js', gulp.parallel('script')) //если были внесены изменения запускает js
-});
+/*----------------------------------------------------*/
+/*	Function that compresses images; 
+------------------------------------------------------*/
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 90, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
+}
 
-gulp.task('build', gulp.series('clean', 'export')); // Таска которая сначала запускает таску clean, а потом таску build
 
-gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch')) //при написании gulp в консоли долно выполняться все что идет после (дефолтная таска)
-                                                                                  // выполниться сначала css потом scss потом js потом запуститься сервер
+
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+/*----------------------------------------------------*/
+/*	Initializes functions in parallel on 'gulp'
+------------------------------------------------------*/
+exports.default = parallel(styles, scripts, browsersync, watching); 
+
+/*----------------------------------------------------*/
+/*	Initializes functions in series (sequence) on 'gulp build'
+------------------------------------------------------*/
+exports.build = series(cleanDist, images, build); 
